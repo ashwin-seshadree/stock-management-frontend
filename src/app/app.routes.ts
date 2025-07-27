a4b7dd0a4041b73c3f1545ce01fb942fda38f6b5
Ashwin Seshadreeswaran <ashwindevelop@gmail.com>
2025-07-27 16:34:31 +0000

    login component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login, title: 'Login' },
+  { path: '**', redirectTo: 'login' },
+];
---
+++ src/app/components/auth/login/login.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  styleUrl: './login.sass',
+})
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      console.log('Logging in with', email, password);
+      // Call auth service here
+    }
+  }
+}
