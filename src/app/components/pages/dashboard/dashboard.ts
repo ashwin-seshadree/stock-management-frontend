03dcabef57d50227b4616138592a0a48d1e0891f
Ashwin Seshadreeswaran <ashwindevelop@gmail.com>
2025-07-31 17:47:27 +0000

    properize the structure.

--- src/app/components/pages/dashboard/dashboard.ts
+++ src/app/components/pages/dashboard/dashboard.ts
@@ -1,29 +1,9 @@
-import { Component, ViewChild, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
-import { Modal } from 'bootstrap';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.sass', '../../pagesstyles.scss']
 })
-export class Dashboard implements AfterViewInit, OnDestroy {
-  @ViewChild('exampleModal') exampleModal!: ElementRef;
+export class Dashboard {
   title: string = 'Dashboard';
-  _exampleModal!: Modal;
-
-  ngAfterViewInit() {
-    this._exampleModal = new Modal(this.exampleModal.nativeElement, {
-      backdrop: 'static',
-      keyboard: false
-    });
-  }
-
-  showModal() {
-    this._exampleModal.show();
-  }
-
-  ngOnDestroy() {
-    if (this._exampleModal) {
-      this._exampleModal.dispose();
-    }
-  }
 }
--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet, RouterLink, ActivatedRoute, Router } from '@angular/router';
 import { SessionService } from '../../services/session.service';
 import { CommonModule } from '@angular/common';
 import { config } from '../../configs/config';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-layout',
@@ -14,18 +15,33 @@ import { config } from '../../configs/config';
 export class Layout {
   appName = config.app_name;
   appVersion = config.app_version;
+  @ViewChild('logoutModal') logoutModal!: ElementRef;
+  _logoutModal!: Modal;
+
   constructor(
     public sessionService: SessionService,
     private router: Router
   ) { }
 
+  ngAfterViewInit() {
+    this._logoutModal = new Modal(this.logoutModal.nativeElement, {
+      backdrop: 'static',
+      keyboard: false
+    });
+  }
 
   isActive(route: string): boolean {
     return this.router.url.includes(route);
   }
 
-  logout() {
-    console.log('Logging out');
-    this.sessionService.endSession();
+  logout(type: number = 1) {
+    if (type == 1) this._logoutModal.show();
+    else this.sessionService.endSession();
+  }
+
+  ngOnDestroy() {
+    if (this._logoutModal) {
+      this._logoutModal.dispose();
+    }
   }
 }
